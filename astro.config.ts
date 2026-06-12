f14cbed5dc16ad14e12a054e1dc46e14c4d0a36b
Simon <wangxujun0516@gmail.com>
2026-06-12 16:20:16 +0800

    Fix: Add MDX support and successfully render infographic image with SEO attributes

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://web.blogx.de5.net",
+  integrations: [mdx()],
   markdown: {
     shikiConfig: {
       theme: "github-light",
