1640e52542b6da3d35cc7a09bf32d32b1f3ae2f5
Simon <wangxujun0516@gmail.com>
2026-06-12 15:12:28 +0800

    Fix: Update page titles and resolve sitemap 404 issues

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://web.blogx.de5.net",
-  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       theme: "github-light",
